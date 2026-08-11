6c3f9e1bd82264009ab84f0e698f67d58ab5126d
David Favela <dfavelava@gmail.com>
2026-08-11 19:54:46 -0400

    Allow writing

--- backend/resources/memoryResource.go
+++ backend/resources/memoryResource.go
@@ -15,6 +15,10 @@ type MemoryResourceImpl struct {
 	s3Client *s3.Client
 }
 
+type CreateMemoryRequest struct {
+	Dir string `json:"dir"`
+}
+
 func NewMemoryResource(r *gin.Engine) *MemoryResourceImpl {
 	return &MemoryResourceImpl{}
 }
@@ -30,6 +34,7 @@ func InitMemoryResource(r *gin.Engine) {
 	resource.s3Client = s3.NewFromConfig(cfg)
 
 	group := r.Group("/memory")
+	group.POST("/", resource.write)
 	group.GET("/:key", resource.read)
 }
 
@@ -62,4 +67,32 @@ func (resource *MemoryResourceImpl) read(c *gin.Context) {
 	}
 
 	c.DataFromReader(200, contentLength, contentType, result.Body, extraHeaders)
+}
+
+func (resource *MemoryResourceImpl) write(c *gin.Context) {
+	fileHeader, err := c.FormFile("file")
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	file, err := fileHeader.Open()
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	defer file.Close()
+
+	_, err = resource.s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String("daybid-dev"),
+		Key:    aws.String(fileHeader.Filename),
+		Body:   file,
+	})
+
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, gin.H{"message": "success"})
 }
